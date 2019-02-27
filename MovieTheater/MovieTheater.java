65d123e7af98cfb5ca3ca4cdc4289451a6b9842a
Aayush Dhiman <aayushdhiman@gmail.com>
2019-02-26 23:40:22 -0500

    Update MovieTheater.java
    update removed binary search in favor of custom one, still doesn't work

--- MovieTheater/MovieTheater.java
+++ MovieTheater/MovieTheater.java
@@ -1,58 +1,49 @@
 package MovieTheater;
 
 public class MovieTheater {
-    private static Comparable[] theater = {false, false, false, false, false, false, false, false};
+    private static boolean[] theater = {false, false, false, false, false, false, false, false};
+    private static int lastSeat = 0;
 
-    public static int binarySearch(Comparable[] array, Comparable target) {
-        int start = 0;
-        int end = array.length - 1;
-        while (start <= end) {
-            int middle = (start + end) / 2;
-            int compared = array[middle].compareTo(target);
-            if (compared == 0) {
-                return middle;
-            } else if (compared > 0) {
-                end = middle - 1;
-            } else {
-                start = middle + 1;
-            }
+    public static int findSeat(int start, int end) {
+        int middle = (start + end) / 2;
 
+        if(!theater[middle]){
+            lastSeat = middle;
+            return middle;
+        }else if(lastSeat - start > end - lastSeat){
+            end = middle - 1;
+            return findSeat(start, end);
+        }else if(lastSeat - start < end - lastSeat){
+            start = middle + 1;
+            return findSeat(start, end);
         }
         return -1;
-
     }
 
-    public static boolean ifFull(){
-        for (int i = 0; i < theater.length; i++) {
-            if(theater[i].equals(false)){
-                return false;
-            }
-        }
-        return true;
-    }
 
-    public static void printSeats(){
-        for (Comparable seat :
-                theater) {
-            if(seat.equals(true)){
+    public static void printSeats() {
+        for (Comparable seat : theater) {
+            if (seat.equals(true)) {
                 System.out.print("X ");
-            }else if(seat.equals(false)){
+            } else if (seat.equals(false)) {
                 System.out.print("_ ");
-            }else{
+            } else {
                 System.out.print(" ");
             }
         }
     }
 
     public static void main(String[] args) {
-        for(int i = 0; i < theater.length; i++){
-            if(theater[i].equals(false)){
-                printSeats();
-                System.out.println();
-                theater[binarySearch(theater, false)] = true;
-            }else if(ifFull()){
-                printSeats();
-                break;
+        int startRow = 0;
+        int endRow = theater.length - 1;
+
+        for (int i = 0; i < theater.length; i++) {
+            if(findSeat(0, theater.length - 1) > 0){
+                if (!theater[i]) {
+                    printSeats();
+                    System.out.println();
+                    theater[findSeat(startRow, endRow)] = true;
+                }
             }
         }
     }
