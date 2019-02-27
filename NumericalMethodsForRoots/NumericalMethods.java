78550540d5d53038ed048d61d6db2c75b7f89ac5
Aayush Dhiman <aayushdhiman@gmail.com>
2019-02-26 23:04:04 -0500

    Update NumericalMethods.java
    Works for multiple equations now. Fixed infinite runtime error.

--- NumericalMethodsForRoots/NumericalMethods.java
+++ NumericalMethodsForRoots/NumericalMethods.java
@@ -1,169 +1,100 @@
 package NumericalMethods;
 
-import java.util.Scanner;
-
 public class NumericalMethods {
-    private static int attempts = 0;
-    private static double tolerance = .000001;
-    private static int eq = 0;
-
-    /**
-     * Evaluates the function
-     *
-     * @param x the point to evaluate the function at
-     * @return the value of the function
-     */
-    public static float function(float x) {
-        if (eq == 1) {
-            return (float) (Math.pow(x, 2) - (5 * x) - 6); // x^2 -5x -6
-        } else if (eq == 2) {
-            return (float) (3 * Math.toDegrees(Math.sin(Math.toRadians(x))) - 5 * Math.toDegrees(Math.cos(Math.toRadians(x))));
-        } else {
-            return (float) (Math.pow(Math.E, x) + Math.pow(x, 2));
-        }
-    }
-
-    /**
-     * Evaluates the derivative of the function
-     *
-     * @param x the value to find the derivative at
-     * @return the value of the derivative
-     */
-    public static float derivative(float x) {
-        if (eq == 1) {
-            return 2 * x - 5; // 2x-5
-        } else if (eq == 2) {
-            return (float) (3 * Math.cos(Math.toRadians(x)) + 5 * Math.sin(Math.toRadians(x)));
-        } else {
-            return (float) (Math.pow(Math.E, x) + 2 * x);
-        }
-    }
-
-    /**
-     * Finds the midpoint of the parameters.
-     *
-     * @param a the first parameter
-     * @param b the second parameter
-     * @return the midpoint of the two points
-     */
-    public static float midPoint(float a, float b) {
-        return (a + b) / 2;
-    }
-
-    /**
-     * Checks to see if the parameters are of opposite signs or not.
-     *
-     * @param x the first parameter
-     * @param y the second parameter
-     * @return the boolean determining whether the parameters have opposite signs or not
-     */
-    public static boolean oppositeSigns(float x, float y) {
-        return (x < 0) ? y >= 0 : (y < 0);
-    }
-
-
-    /**
-     * Uses the bisection method to narrow down the location of the root of the function given the
-     * domain of the function and the guarantee of the Intermediate Value Theorum.
-     *
-     * @param a the lower bound of the domain
-     * @param b the upper bound of the domain
-     * @return the nearest root of the function
-     */
-    public static float bisectionMethod(float a, float b) {
-        float c = midPoint(a, b);
-        while (Math.abs(function(c)) > tolerance) {
-            if (oppositeSigns(function(c), function(a))) {
-                b = c;
-            } else {
-                a = c;
-            }
-            attempts++;
-            c = midPoint(a, b);
-        }
-        return c;
-    }
-        /*
-        while (Math.abs(function(midPoint)) > close) {
-      bisectionTrials++;
-      if (function(midPoint) * function(a) > 0) a = midPoint;
-      else b = midPoint;
-      midPoint = (a + b) / 2;
-    }
-    return midPoint;
-         */
-
-    /**
-     * Uses Newton's Method to find the root of a function, given the domain of the function and the guarantee of the
-     * Intermediate Value Theorum.
-     *
-     * @param x0 the guess of the location of the root
-     * @return the location of the nearest root of the function
-     */
-    public static double newtonsMethod(float x0) {
-        float xN = x0 - (function(x0) / derivative(x0));
-        if (tolerance >= function(xN) && function(xN) >= -tolerance) {
-            attempts++;
-            return xN;
-        } else {
-            attempts++;
-            return newtonsMethod(xN);
-        }
-    }
-
-    /**
-     * Uses the secant method of drawing secant lines to narrow down the location of the root of the function given the
-     * domain of the function and the guarantee of the Intermediate Value Theorum.
-     *
-     * @param x0 the lower bound of the domain
-     * @param x1 the upper bound of the domain
-     * @return the nearest root of the function
-     */
-    public static float secantMethod(float x0, float x1) {
-        float x2 = (x0 * function(x1) - x1 * function(x0)) / (function(x1) - function(x0));
-        if (tolerance >= function(x2) && -tolerance <= function(x2)) {
-            attempts++;
-            return x2;
-        } else {
-            attempts++;
-            return secantMethod(x1, x2);
-        }
-    }
-
-    public static void main(String args[]) {
-        for (eq = 1; eq < 4; eq++) {
-            if (eq == 1) {
-                attempts = 0;
-                System.out.println("bisection:\tx: " + bisectionMethod(-4, 5) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("netwon:\tx: " + newtonsMethod(-7) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("secant:\tx: " + secantMethod(-4, 5) + ",\titerations: " + attempts);
-                System.out.println("=============================================\n");
-
-
-            } else if (eq == 2) {
-                attempts = 0;
-                System.out.println("bisection:\tx: " + bisectionMethod(-1, 3) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("netwon:\tx: " + newtonsMethod(-7) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("secant:\tx: " + secantMethod(-1, 3) + ",\titerations: " + attempts);
-                System.out.println("=============================================\n");
-
-            } else {
-                attempts = 0;
-                System.out.println("bisection:\tx: " + bisectionMethod(-2, 2) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("netwon:\tx: " + newtonsMethod(1) + ",\titerations: " + attempts);
-                attempts = 0;
-                System.out.println("secant:\tx: " + secantMethod(-2, 2) + ",\titerations: " + attempts);
-                System.out.println("=============================================\n");
-
-            }
-
-        }
-
-    }
-
+	private static double tolerance = .000001;
+	private static int bisectionIterations = 0;
+	private static int newtonsIterations = 0;
+	private static int secantsIterations = 0;
+	private static int equationNum = 0;
+	
+	public static void main(String args[]) {
+		System.out.println("Equation: x^2 - 5x - 6");
+		runAllTests(1, -4, 5);
+		
+		System.out.println("Equation: x^3 + 4x^2 + 2");
+		runAllTests(2,  -8, 5);
+		
+		System.out.println("Equation: 3cos(x) - 2sin(x)");
+		runAllTests(3,  0, 2);
+		
+		System.out.println("Equation: e^x + x^2 - 5");
+		runAllTests(4, 0, 2);
+		
+	}
+	
+	public static void runAllTests(int eq, int a, int b) {
+		bisectionIterations = 0;
+		newtonsIterations = 0;
+		secantsIterations = 0;
+		equationNum = eq;
+		System.out.println(
+				"bisectionMethod: x = " + bisectionMethod(a, b) + "\titerations:  " + bisectionIterations);
+		System.out.println(
+				"newtonMethod: x = " + newtonMethod(a) + "\titerations: " + newtonsIterations);
+		System.out.println(
+				"secantMethod: x = " + secantMethod(a, b) + "\titerations: " + secantsIterations);
+		System.out.println("=======================================================\n");
+	}
+	
+	public static double function(double x) {
+		switch (equationNum) {
+			case 1:
+				return Math.pow(x, 2) - (5 * x) - 6; // x^2 -5x -6
+			case 2:
+				return Math.pow(x, 3) + 4 * (Math.pow(x, 2)) + 2; // x^3 +4x^2 +2
+			case 3:
+				return (3 * Math.cos(x)) - (2 * Math.sin(x)); // 3cos(x) - 2sin(x)
+			case 4:
+				return (Math.pow(Math.E, x)) + (Math.pow(x, 2)) - 5; // e^x + x^2 -5
+			default:
+				return Math.pow(x, 2) - (5 * x) - 6; // x^2 -5x -6
+		}
+	}
+	
+	public static double derivative(double x) {
+		switch (equationNum) {
+			case 1:
+				return 2 * x - 5; // 2x-5
+			case 2:
+				return 3 * (Math.pow(x, 2)) + 8 * x; // 3x^2+8x
+			case 3:
+				return (-3 * Math.sin(x)) - (2 * Math.cos(x)); // -3sin(x) - 2cos(x)
+			case 4:
+				return (Math.pow(Math.E, x)) + 2 * x; // e^x + 2x
+			default:
+				return Math.pow(x, 2) - (5 * x) - 6; // x^2 -5x -6
+		}
+	}
+	
+	public static double bisectionMethod(double a, double b) {
+		double midPoint = (a + b) / 2;
+		while (Math.abs(function(midPoint)) > tolerance) {
+			bisectionIterations++;
+			if (function(midPoint) * function(a) > 0) a = midPoint;
+			else b = midPoint;
+			midPoint = (a + b) / 2;
+		}
+		return midPoint;
+	}
+	
+	public static double newtonMethod(double x0) {
+		newtonsIterations++;
+		double x1 = x0 - (function(x0) / derivative(x0));
+		if (tolerance >= function(x1) && function(x1) >= -tolerance) {
+			return x1;
+		} else {
+			return newtonMethod(x1);
+		}
+	}
+	
+	public static double secantMethod(double x1, double x2) {
+		secantsIterations++;
+		double x0 = (x1 * function(x2) - x2 * function(x1)) / (function(x2) - function(x1));
+		if (tolerance >= function(x0) && function(x0) >= -tolerance) {
+			return x0;
+		} else {
+			return secantMethod(x2, x0);
+		}
+	}
 }
+
